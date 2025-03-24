48518db4f8fca1fe878d3c7a8e4ef158cf95b639
Suryabhan Singh <suryabhan283808@gmail.com>
2025-03-24 17:57:54 +0530

    Add global styles and enhance navbar with transitions; improve button and card hover effects

--- kirana/src/main.jsx
+++ kirana/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
+import './Css/global.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
