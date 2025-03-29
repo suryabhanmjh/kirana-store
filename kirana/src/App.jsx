03fc64b4bb6ba1fbfa67c8ee47f216cc34cfebb8
Suryabhan Singh <suryabhan283808@gmail.com>
2025-03-29 11:21:21 +0530

    Add app container styles and refactor App component layout for improved structure

--- kirana/src/App.jsx
+++ kirana/src/App.jsx
@@ -5,23 +5,25 @@ import Home from "./components/Home";
 import Products from "./components/Products";
 import Cart from "./components/Cart";
 import Checkout from "./components/Checkout";
+import "./Css/app.css";
 
 function App() {
   const [cart, setCart] = useState([]);
 
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products setCart={setCart} />} />
-          <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
-          <Route path="/checkout" element={<Checkout cart={cart} />} />
-
-        </Routes>
-      </Router>
-    </>
+    <div className="app-container">
+      <>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products setCart={setCart} />} />
+            <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
+            <Route path="/checkout" element={<Checkout cart={cart} />} />
+          </Routes>
+        </Router>
+      </>
+    </div>
   );
 }
 
